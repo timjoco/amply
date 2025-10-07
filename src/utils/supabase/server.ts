945cd1da770a5b6678f5a7fe5294427587bc9e74
Tim <timjoco13@gmail.com>
2025-10-07 14:36:42 -0500

    fixed supabase server config

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -17,11 +17,7 @@ export async function createClient() {
             cookiesToSet.forEach(({ name, value, options }) =>
               cookieStore.set(name, value, options)
             );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+          } catch {}
         },
       },
     }
