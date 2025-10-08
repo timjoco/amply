0e2172f91a2d38d4d6a6ddc098baedadc2e6296f
Tim <timjoco13@gmail.com>
2025-10-08 12:04:59 -0500

    created route for user settings

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export const dynamic = 'force-dynamic';
+import { Box, Typography } from '@mui/material';
+
+export default function DashboardPage() {
+  return (
+    <Box>
+      <Typography>SETTINGS</Typography>
+    </Box>
+  );
+}
