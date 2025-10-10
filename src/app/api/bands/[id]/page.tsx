cfbeda2cb6ad9201ea8e6160c11ac517dd7ceb79
Tim <timjoco13@gmail.com>
2025-10-10 11:58:33 -0500

    chore(api): remove legacy invite pages and routes

--- src/app/api/bands/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-import BandSheet from '@/components/Bands/BandSheet';
-
-type Params = Promise<{ id: string }>;
-
-export default async function BandPage({ params }: { params: Params }) {
-  const { id } = await params;
-  return <BandSheet bandId={id} />;
-}
