16041db1d8c31254394b9cd23160fa1b94d80900
Tim <timjoco13@gmail.com>
2025-10-10 16:41:14 -0500

    testing styles on PC to fix bug

--- src/components/ThemeClient.tsx
+++ src/components/ThemeClient.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import theme from '@/theme';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
 
 export default function ThemeClient({
   children,
@@ -11,6 +11,15 @@ export default function ThemeClient({
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+      <GlobalStyles
+        styles={(t) => ({
+          'html, body, #__next, body > div:first-of-type': {
+            minHeight: '100%',
+            backgroundColor: `${t.palette.background.default} !important`,
+          },
+          body: { margin: 0 },
+        })}
+      />
       {children}
     </ThemeProvider>
   );
